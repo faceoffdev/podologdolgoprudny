1a26c46d2d1920bf193aef10a0c1dc7b50ad93f6
Вадим Сушинский <faceoff.dev@gmail.com>
2026-01-31 18:03:31 +0300

    Deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/podologdolgoprudny',
+  assetPrefix: '/podologdolgoprudny',
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
